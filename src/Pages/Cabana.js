3984c8b00e4b610dd3577fcfb99b64f4018b6f3a
Abraham Trujillo <trujilloscompany@hotmail.com>
2021-06-22 14:40:59 -0500

    Fixed some typos and added some text

--- src/Pages/Cabana.js
+++ src/Pages/Cabana.js
@@ -110,7 +110,7 @@ export default function IconButtons() {
       <br />
 
       <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
-        The complete system has different parts, the client, CMS, database and server. Everything resides inside some docker containers hosted on a VPS running linux. The server uses Nginx to serve static conntent and as a reverse proxy to manage incomming connections, all incoming/outgoing connections are encrypted with ssl and has location based bans for countries that are not in the list.
+        The complete system has different parts, the client, CMS, database and server. Everything resides inside Docker containers hosted on a VPS running linux. The server uses Nginx to serve static conntent and as a reverse proxy to manage incomming connections, all incoming/outgoing connections are encrypted with SSL and has location based bans for countries that are not in the list.
       </Typography>
 
       <br />
@@ -134,7 +134,11 @@ export default function IconButtons() {
       </Typography>
       
       <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
-        The client app is built with React.js and GraphQl and has a service worker to make it a PWA so customers can install it on their devices. The app calls the database using GraphQl queries which are cached for faster response time. This app also has Push Notifications which are essential for recieving order updates and promotrions.
+        The client app is built with React.js and GraphQL and has a service worker to make it a PWA so customers can install it on their devices. The app calls the database using GraphQL queries which are cached for faster response time. This app also has Push Notifications which are essential for recieving order updates, promotions and important messages.
+      </Typography>
+
+      <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
+        It has categories and nested subcategories, currentrly only two levels of nested data can be displayed but it supports infinite sublevels of nested data.
       </Typography>
 
       <br />
@@ -161,7 +165,7 @@ export default function IconButtons() {
       </Typography>
 
       <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
-        The CMS is built aslo with React.js and GraphQl and has PWA capabilities but is mainly purposed to use it with biger devices. The app calls the database using GraphQl queries and mutations. It has everything one needs to CRUD data for this system so the client can display all managed data.
+        The CMS is built aslo with React.js and GraphQl and has PWA capabilities and is mainly purposed to use it with biger devices. The app calls the database using GraphQl queries and mutations. It has everything one needs to CRUD data for this system so the client can display all managed data. 
       </Typography>
 
       <br />
@@ -190,7 +194,10 @@ export default function IconButtons() {
         Database
       </Typography>
       <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
-        The database is built using Postgresql, and is managed with a Node.js server with GraphQL schemas that deal with connections and data.
+        The database is built using Postgresql, and is managed with a Node.js server with GraphQL schemas that deal with connections and data. It only stores the App specific data and not user data. In case the database is compromised for some reason, the user data is secure stored somwhere else.
+      </Typography>
+      <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
+        All the user data is stored inside a Firebase proyect and is only accessible by it's owner being private to the user. The Firebase rules are set to only display data to an authenticated user and only it's corresponding data.
       </Typography>
 
       <br />
@@ -199,7 +206,7 @@ export default function IconButtons() {
         Server
       </Typography>
       <Typography component="p" variant="body1" className={classes.body1} color="textSecondary" gutterBottom>
-        There are 2 servers in the system, Nginx for static files, connection management and reverse proxy and Node.js that deals with the database stuff and some tooling for image processing. 
+        There are 2 servers in the system, Nginx for static files, connection management and reverse proxy and a Node.js server that deals with database queries, some tooling for image processing and user authentication.
       </Typography>
 
       <Separator />
